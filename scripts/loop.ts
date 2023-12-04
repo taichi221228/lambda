a07cf93ca3b6022d45ea437da5d51808901dbcf5
taichi221228 <taichi221228@icloud.com>
2023-12-04 18:42:29 +0900

    Switch implementation of `sum()` to split assignment.
    Change the implementation of `sum()` to split assignment for readability.

--- scripts/loop.ts
+++ scripts/loop.ts
@@ -1,5 +1,5 @@
-const sum = (arr: number[], acc = 0): number => {
-    return (arr.length === 0) ? acc : sum(arr.slice(1), acc + arr[0]);
+const sum = ([n, ...rest]: number[]): number => {
+    return (n === undefined) ? 0 : n + sum(rest);
 }
 
 const main = () => {
