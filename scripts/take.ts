83ba9f5ffc6e4be7559523d73421978b416c8b49
taichi221228 <taichi221228@icloud.com>
2023-12-12 18:54:22 +0900

    Refactor 'take' function to avoid 'head' function dependency
    Updated 'take' function in scripts/take.ts to remove the dependency on 'head' function. Instead, array index is directly used for better code simplicity and maintainability; also reducing reliance on external functions.

--- scripts/take.ts
+++ scripts/take.ts
@@ -1,5 +1,4 @@
-import { head } from "./head.ts";
 import { tail } from "./tail.ts";
 
 export const take = <T>(x: number, arr: T[]): T[] =>
-  x === 0 ? [] : [head(arr), ...take(x - 1, tail(arr))];
+  x === 0 ? [] : [arr[0], ...take(x - 1, tail(arr))];
