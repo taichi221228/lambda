6e7c4dc54100ae5064267159b825f60604fbcbc1
taichi221228 <taichi221228@icloud.com>
2023-12-14 17:59:01 +0900

    Add 'max.ts' example to demonstrate usage of max function
    The commit introduces a new file 'max.ts' under /examples. This example script calls the 'max' function from /scripts on a predefined set of numbers to demonstrate its usage and output. The script has error handling for cases where an empty array is provided.

---
+++ examples/max.ts
@@ -0,0 +1,11 @@
+import { numbers } from "../constants/numbers.ts";
+import { max } from "../scripts/max.ts";
+
+const main = () => {
+  /**
+   * @returns {5}
+   */
+  console.log(max(numbers));
+};
+
+main();
