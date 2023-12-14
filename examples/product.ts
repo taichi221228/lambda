95192fb81a35de8bbf8304af57ad7e9f46b0b781
taichi221228 <taichi221228@icloud.com>
2023-12-14 16:25:13 +0900

    Add 'product.ts' script with product calculation function
    This commit introduces a new script, 'product.ts', to the examples directory. The script is used to calculate the product of an array of numbers. Additionally, special handling for empty arrays has been implemented to mitigate potential undefined attribute issues.

---
+++ examples/product.ts
@@ -0,0 +1,11 @@
+import { numbers } from "../constants/numbers.ts";
+import { product } from "../scripts/product.ts";
+
+const main = () => {
+  /**
+   * @returns {120}
+   */
+  console.log(product(numbers));
+};
+
+main();
