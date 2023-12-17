5ef7149e6fcbd2b370faba7ff5737907105629b8
taichi221228 <taichi221228@icloud.com>
2023-12-17 17:06:31 +0900

    Refactor quicksort function's pivot logic in qsort.ts
    Simplified the pivot logic used in the quicksort implementation on qsort.ts file. The pivot handling has been modified to remove the separated 'pivot' const declaration for cleaner, more readable, and streamlined logic.

--- scripts/qsort/qsort.ts
+++ scripts/qsort/qsort.ts
@@ -3,9 +3,8 @@ import { filter } from "$scripts/filter/filter.ts";
 export const qsort = ([n, ...ns]: number[]): number[] =>
   n === undefined
     ? []
-    : (() => {
-        const pivot = [n];
-        const smaller = filter((a) => a <= n, ns);
-        const larger = filter((b) => b > n, ns);
-        return [...qsort(smaller), ...pivot, ...qsort(larger)];
-      })();
+    : [
+        ...qsort(filter((a) => a <= n, ns)),
+        n,
+        ...qsort(filter((b) => b > n, ns)),
+      ];
