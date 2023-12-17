4ee69dbecec9e4b0fecfd4ee0af82036af149807
taichi221228 <taichi221228@icloud.com>
2023-12-17 15:43:16 +0900

    Update "max" function parameters in max.ts
    The extra argument "_: boolean = false" in the "max" function signature has been removed. This modification simplifies the function invocation and makes the code more readable and maintainable.

--- scripts/max/max.ts
+++ scripts/max/max.ts
@@ -1,2 +1,2 @@
-export const max = ([x, ...xs]: number[], _: boolean = false): number =>
+export const max = ([x, ...xs]: number[]): number =>
   x === undefined ? Number.MIN_SAFE_INTEGER : x > max(xs) ? x : max(xs);
