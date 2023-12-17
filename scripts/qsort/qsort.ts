2a749360f172e2b95fd74785337626b6df1af9fb
taichi221228 <taichi221228@icloud.com>
2023-12-17 17:03:51 +0900

    Refactor qsort to use custom filter function
    A separate "filter" function was imported and implemented in the quicksort ts file. This change enhances the function readability, modularity, and avoids code redundancy by leveraging a custom filter function instead of the native JavaScript filter function.

--- scripts/qsort/qsort.ts
+++ scripts/qsort/qsort.ts
@@ -1,9 +1,11 @@
+import { filter } from "$scripts/filter/filter.ts";
+
 export const qsort = ([n, ...ns]: number[]): number[] =>
   n === undefined
     ? []
     : (() => {
         const pivot = [n];
-        const smaller = ns.filter((a) => a <= n);
-        const larger = ns.filter((b) => b > n);
+        const smaller = filter((a) => a <= n, ns);
+        const larger = filter((b) => b > n, ns);
         return [...qsort(smaller), ...pivot, ...qsort(larger)];
       })();
