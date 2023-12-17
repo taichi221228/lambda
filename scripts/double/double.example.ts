9dc4bfc1072a1f41049bbea7088896a779c4f78b
taichi221228 <taichi221228@icloud.com>
2023-12-17 17:21:59 +0900

    Refactor double usage in main function
    The usage of the double function within the main function has been adjusted to directly pass a numeric value instead of referencing an array value. This refactoring simplifies the function call in 'double.example.ts' and enhances the code readability.

--- scripts/double/double.example.ts
+++ scripts/double/double.example.ts
@@ -1,11 +1,10 @@
-import { numbers } from "$constants/numbers.ts";
 import { double } from "./double.ts";
 
 const main = () => {
   /**
    * @returns {2}
    */
-  console.log(double(numbers[0]));
+  console.log(double(3));
 };
 
 main();
