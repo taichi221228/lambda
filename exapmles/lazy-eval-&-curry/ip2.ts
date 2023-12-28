aa70c05ea8b95309c4a0f0c91bd7205bcfc3b143
taichi221228 <taichi221228@icloud.com>
2023-12-28 11:01:23 +0900

    Refactor fetch data output formatting
    The data fetched from "https://jsonplaceholder.typicode.com/todos" is now being formatted into a new 'formattedTodos' constant. The formatted data will only include 'completed' todos and will be logged in the console. This change will improve the straightforwardness and readability of the output data.

--- exapmles/lazy-eval-&-curry/ip2.ts
+++ exapmles/lazy-eval-&-curry/ip2.ts
@@ -3,13 +3,18 @@ import { Todo, TodoFormatted } from "./type.ts";
 const res = await fetch("https://jsonplaceholder.typicode.com/todos");
 const todos = await res.json<Todo[]>();
 
-todos.reduce((acc: TodoFormatted[], { id, title, completed, ...rest }) => {
-  if (completed) {
-    acc.push({
-      title: `${id} - ${title}`,
-      completed,
-      ...rest,
-    });
-  }
-  return acc;
-}, []);
+const formattedTodos = todos.reduce(
+  (acc: TodoFormatted[], { id, title, completed, ...rest }) => {
+    if (completed) {
+      acc.push({
+        title: `${id} - ${title}`,
+        completed,
+        ...rest,
+      });
+    }
+    return acc;
+  },
+  [],
+);
+
+console.log(formattedTodos);
