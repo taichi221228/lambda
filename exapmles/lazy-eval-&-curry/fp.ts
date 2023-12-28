841227be2f5baad3baec2409df935783c7a4a30d
taichi221228 <taichi221228@icloud.com>
2023-12-28 11:25:23 +0900

    Add functional programming methods to 'fp.ts'
    Added methods for creating filter and mapping functions in the 'fp.ts' file. These methods will facilitate task handling where

---
+++ exapmles/lazy-eval-&-curry/fp.ts
@@ -0,0 +1,44 @@
+import { Todo, TodoFormatted } from "./type.ts";
+
+const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+const todos = await res.json<Todo[]>();
+
+function createFilter<T>(
+  predicate: (item: T) => boolean,
+): (sequence: T[]) => Generator<T, void, unknown> {
+  return (sequence: T[]) => {
+    function* filter() {
+      for (const item of sequence) {
+        if (predicate(item)) {
+          yield item;
+        }
+      }
+    }
+
+    return filter();
+  };
+}
+
+function createMap<T, R>(
+  mapper: (sequence: T) => R,
+): (sequence: Generator<T, void, unknown>) => Generator<R, void, unknown> {
+  return (sequence: Generator<T, void, unknown>) => {
+    function* map() {
+      for (const item of sequence) {
+        yield mapper(item);
+      }
+    }
+
+    return map();
+  };
+}
+
+const completeFilter = createFilter<Todo>((todo) => todo.completed);
+const todoMapper = createMap<Todo, TodoFormatted>(({ id, title, ...rest }) => ({
+  title: `${id} - ${title}`,
+  ...rest,
+}));
+
+const formattedTodos = Array.from(todoMapper(completeFilter(todos)));
+
+console.log(formattedTodos);
